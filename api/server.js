bbed932732f77642cde4cbadde21432247cb17ca
Victor Brambila <victorbrambila22@gmail.com>
2022-04-14 14:20:01 -0700

    added env

--- api/server.js
+++ api/server.js
@@ -22,7 +22,7 @@ const users = [
 ]
 
 server.get('/', (req, res) => {
-    res.json({ MESSAGE: process.env.MESSAGE });
+    res.json({ MESSAGE: process.env.MESSAGE, INSTRUCTIONS: process.env.INSTRUCTIONS });
 });
 
 server.get('/api/users', (req, res) => {
