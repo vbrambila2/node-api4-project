e31d5f4407291a89623797eabb4918e891366356
Victor Brambila <victorbrambila22@gmail.com>
2022-04-14 14:16:29 -0700

    GET test

--- api/server.js
+++ api/server.js
@@ -6,20 +6,35 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
+const users = [
+    {
+        id: 1,
+        name: "vic"
+    },
+    {
+        id: 2,
+        name: "cal"
+    },
+    {
+        id: 3,
+        name: "joe"
+    }
+]
+
 server.get('/', (req, res) => {
     res.json({ MESSAGE: process.env.MESSAGE });
 });
 
-// server.get('/api/users', (req, res) => {
-//     res.json('get');
-// });
+server.get('/api/users', (req, res) => {
+    res.json(users);
+});
 
-// server.post('/api/register', (req, res) => {
-//     res.json('post');
-// });
+server.post('/api/register', (req, res) => {
+    res.json('post');
+});
 
-// server.post('/api/login', (req, res) => {
-//     res.json('postTwo');
-// });
+server.post('/api/login', (req, res) => {
+    res.json('postTwo');
+});
 
 module.exports = server;
