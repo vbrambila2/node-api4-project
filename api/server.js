a17d78990468a6e0958144fa808d5a102f2eb9ab
Victor Brambila <victorbrambila22@gmail.com>
2022-04-14 13:53:27 -0700

    initial set up done

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+    res.json({ MESSAGE: process.env.MESSAGE });
+});
+
+module.exports = server;
